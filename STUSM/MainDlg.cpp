e983a56388da585e8eea4cccf7d03fe4d072b2eb
LinFeng <LinFeng@github.com>
2022-07-28 13:05:57 +0800

    添加注释 完善代码

--- STUSM/MainDlg.cpp
+++ STUSM/MainDlg.cpp
@@ -4,12 +4,7 @@
 #include "STUSM.h"
 #include "MainDlg.h"
 #include "afxdialogex.h"
-#include "Student.h"
-#include "Management.h"
 
-//声明全局变量
-Management ManagerSystem;	//管理系统的对象
-int SortID;	//	用于观察当前列表状态（正常/按高数/按CPP）
 
 // MainDlg 对话框
 
@@ -26,7 +21,7 @@ MainDlg::MainDlg(CWnd* pParent /*=NULL*/)
 	, m_CppT(_T(""))
 	, m_STU(_T(""))
 {
-
+	m_Row = -1;
 }
 
 MainDlg::~MainDlg()
@@ -68,6 +63,7 @@ BEGIN_MESSAGE_MAP(MainDlg, CDialogEx)
 	ON_BN_CLICKED(IDC_RADIO_Gra2, &MainDlg::OnBnClickedRadioGra2)
 	ON_BN_CLICKED(IDC_RADIO_Gra3, &MainDlg::OnBnClickedRadioGra3)
 	ON_BN_CLICKED(IDC_RADIO_Gra4, &MainDlg::OnBnClickedRadioGra4)
+	ON_NOTIFY(NM_DBLCLK, IDC_LIST_Show, &MainDlg::OnNMDblclkListShow)
 END_MESSAGE_MAP()
 
 
@@ -112,7 +108,7 @@ BOOL MainDlg::OnInitDialog()
 	//	m_List.SetItemText(i, 5, math);
 	//	m_List.SetItemText(i, 6, cpp);
 	//}
-
+	
 	//更新学生人数
 	m_STU.Format(_T("%d"), m_List.GetItemCount());
 	SetDlgItemText(IDC_EDIT_STU, m_STU);
@@ -186,6 +182,17 @@ void MainDlg::OnBnClickedRadioGra4()
 }
 
 
+//	“排序”单选框
+void MainDlg::OnBnClickedRadiomaths()
+{
+	m_Sort = 0;
+}
+void MainDlg::OnBnClickedRadioCpp()
+{
+	m_Sort = 1;
+}
+
+
 //	点击“全选”按钮
 void MainDlg::OnBnClickedButtonAll()
 {
@@ -209,7 +216,7 @@ void MainDlg::OnBnClickedMfcbuttonAdd()
 	double cpp =0.4* _tcstod(m_CppD, NULL) + 0.6*_tcstod(m_CppT, NULL);
 	
 	//	判断是否获取了空数据
-	if (m_Name && m_Num && m_CppD && m_CppT && m_MathsD && m_MathsT && m_Gra && m_Sex)
+	if (!(m_Name && m_Num && m_CppD && m_CppT && m_MathsD && m_MathsT && m_Gra && m_Sex))
 	{
 		MessageBox(_T("数据不能为空！请检查输入"), _T("提示"), MB_ICONWARNING);
 		return;
@@ -224,7 +231,13 @@ void MainDlg::OnBnClickedMfcbuttonAdd()
 	temp.Name = m_Name;
 	temp.Sex = m_Sex;
 	temp.Class = m_Class;
+
+	temp.mathsD = m_MathsD;
+	temp.mathsT = m_MathsT;
 	temp.maths = maths;
+
+	temp.CPPD = m_CppD;
+	temp.CPPT = m_CppT;
 	temp.CPP = cpp;
 
 	//将数据检查合理性后放进Managerment的对象ManagerSystem内
@@ -247,7 +260,6 @@ void MainDlg::OnBnClickedMfcbuttonAdd()
 		m_temp.Format(_T("%.1f"), cpp);
 		m_List.SetItemText(nCount, 6, m_temp);
 
-
 		//更新学生人数
 		m_STU.Format("%d", nCount+1);
 		SetDlgItemText(IDC_EDIT_STU, m_STU);
@@ -259,68 +271,295 @@ void MainDlg::OnBnClickedMfcbuttonAdd()
 //	点击"编辑"按钮
 void MainDlg::OnBnClickedButtonEdit()
 {
-	//	寻找被选中的行号
-	int i;	//	行号
-
-	//	循环直到找到被勾选项
-	for (i = 0; i < m_List.GetItemCount(); i++)
+	//	寻找被选中的行号 循环直到找到被勾选项
+	for (m_Row = 0; m_Row < m_List.GetItemCount(); m_Row++)
 	{
-		if (m_List.GetCheck(i))
+		if (m_List.GetCheck(m_Row))
 			break;
 	}
 
 	//	如果i=行数 说明用户没有勾选
-	if (i == m_List.GetItemCount())
+	if (m_Row == m_List.GetItemCount())
 	{
 		MessageBox(_T("请勾选一行！"), _T("提示"), MB_ICONWARNING);
 		return;
 	}
 
-	Student temp;
-	CString  m_temp;
+	//	从编辑框获取数据
+	UpdateData(TRUE);
+	double maths = 0.4 * _tcstod(m_MathsD, NULL) + 0.6 * _tcstod(m_MathsT, NULL);
+	double cpp = 0.4 * _tcstod(m_CppD, NULL) + 0.6 * _tcstod(m_CppT, NULL);
 
-	//	将该学生的信息设置到输入框方便进行修改
+	//	判断是否有空数据
+	if (!(m_Name && m_Num && m_CppD && m_CppT && m_MathsD && m_MathsT && m_Gra && m_Sex))
+	{
+		MessageBox(_T("有数据为空！请检查输入"), _T("提示"), MB_ICONWARNING);
+		return;
+	}
+
+	//	获取数据放入临时对象
+	temp.Num = m_Num;
+	temp.Grade = m_Gra;
+	temp.Name = m_Name;
+	temp.Sex = m_Sex;
+	temp.Class = m_Class;
+	temp.CPP = cpp;
+	temp.maths = maths;
+
+	//	检查编辑后数据合理性
+	if (!(ManagerSystem.Iscorrect(temp)))
+	{
+		MessageBox(_T("学号雷同或成绩输入错误！请检查输入"), _T("提示"), MB_ICONWARNING);
+		return;
+	}
+	
+	//	利用管理系统的函数编辑容器
+	ManagerSystem.EditData(temp, m_Row);
+	
+	//	将控件原来行删除并插入修改后的行
+	m_List.DeleteItem(m_Row);
+	m_List.InsertItem(m_Row, m_Num);
+	m_List.SetItemText(m_Row, 1, m_Gra);
+	m_List.SetItemText(m_Row, 2, m_Class);
+	m_List.SetItemText(m_Row, 3, m_Name);
+	m_List.SetItemText(m_Row, 4, m_Sex);
+	//	将浮点型转为cstring型
+	m_temp.Format(_T("%.1f"), maths);
+	m_List.SetItemText(m_Row, 5, m_temp);
+	m_temp.Format(_T("%.1f"), cpp);
+	m_List.SetItemText(m_Row, 6, m_temp);
+}
+
+//	点击"删除"按钮
+void MainDlg::OnBnClickedButtonDelete()
+{
+	//	利用循环逐个找到选中的行进行删除
+	for (m_Row = 0; m_Row < m_List.GetItemCount(); m_Row++)
+	{
+		if (m_List.GetCheck(m_Row))
+		{
+			ManagerSystem.DeleteData(m_Row);
+			m_List.DeleteItem(m_Row);
+			m_Row--;
+		}
+	}
+
+	//	更新学生人数
+	int nCount = m_List.GetItemCount();
+	m_STU.Format(_T("%d"), nCount);
+	SetDlgItemText(IDC_EDIT_STU, m_STU);
+}
+
+//	点击"排序"按钮
+void MainDlg::OnBnClickedButtonSort()
+{
+	//	删除list control全部项目
+	m_List.DeleteAllItems();
+
+	//	获取当前列表总行数
+	int nCount = ManagerSystem.GetCount();
+
+	//	根据m_Sort执行不同的排序分支
+	switch (m_Sort)
+	{
+	case 0:
+		ManagerSystem.SortDataM();
+		
+		//	将排序后的数据输出到列表中
+		for (m_Row = 0; m_Row < nCount; m_Row++)
+		{
+			m_List.InsertItem(m_Row, ManagerSystem.SortM[m_Row].Num);
+			m_List.SetItemText(m_Row, 1, ManagerSystem.SortM[m_Row].Grade);
+			m_List.SetItemText(m_Row, 2, ManagerSystem.SortM[m_Row].Class);
+			m_List.SetItemText(m_Row, 3, ManagerSystem.SortM[m_Row].Name);
+			m_List.SetItemText(m_Row, 4, ManagerSystem.SortM[m_Row].Sex);
+			m_temp.Format(_T("%.1f"), ManagerSystem.SortM[m_Row].maths);
+			m_List.SetItemText(m_Row, 5, m_temp);
+			m_temp.Format(_T("%.1f"), ManagerSystem.SortM[m_Row].CPP);
+			m_List.SetItemText(m_Row, 6, m_temp);
+		}
+
+	case 1:
+		ManagerSystem.SortDataC();
+
+		//	将排序后的数据输出到列表中
+		for (m_Row = 0; m_Row < nCount; m_Row++)
+		{
+			m_List.InsertItem(m_Row, ManagerSystem.SortC[m_Row].Num);
+			m_List.SetItemText(m_Row, 1, ManagerSystem.SortC[m_Row].Grade);
+			m_List.SetItemText(m_Row, 2, ManagerSystem.SortC[m_Row].Class);
+			m_List.SetItemText(m_Row, 3, ManagerSystem.SortC[m_Row].Name);
+			m_List.SetItemText(m_Row, 4, ManagerSystem.SortC[m_Row].Sex);
+			m_temp.Format(_T("%.1f"), ManagerSystem.SortC[m_Row].maths);
+			m_List.SetItemText(m_Row, 5, m_temp);
+			m_temp.Format(_T("%.1f"), ManagerSystem.SortC[m_Row].CPP);
+			m_List.SetItemText(m_Row, 6, m_temp);
+		}
+
+	default:
+		break;
+	}
+
+	//	更新学生人数
+	m_STU.Format(_T("%d"), m_List.GetItemCount());
+	SetDlgItemText(IDC_EDIT_STU, m_STU);
+}
+
+//	点击"保存"按钮
+void MainDlg::OnBnClickedButtonSave()
+{
+	//	设置保存文件对话框的信息
+	CFileDialog fDlg(FALSE, ".dat", "save", OFN_OVERWRITEPROMPT, "数据文件(*.dat)|*.dat", NULL);
+
+	if (fDlg.DoModal()==IDOK)
+	{
+		//	获取文件路径
+		CString sPath = fDlg.GetPathName();
+
+		//	创建文件对象
+		CStdioFile file(sPath, CFile::modeWrite | CFile::modeCreate);
+
+		//	利用for循环将记录写入文件
+		for (m_Row = 0; m_Row < m_List.GetItemCount(); m_Row++)
+		{
+			//	将每列的数据拼接成一行字符串
+			m_temp = '\0';
+			m_temp += m_List.GetItemText(m_Row, 0) + "#";
+			m_temp += m_List.GetItemText(m_Row, 1) + "#";
+			m_temp += m_List.GetItemText(m_Row, 2) + "#";
+			m_temp += m_List.GetItemText(m_Row, 3) + "#";
+			m_temp += m_List.GetItemText(m_Row, 4) + "#";
+			m_temp += m_List.GetItemText(m_Row, 5) + "#";
+			m_temp += m_List.GetItemText(m_Row, 6) + "#\n";
+
+			//	向文件写入一行记录
+			file.WriteString(m_temp);
+		}
+
+		//	写入完毕 关闭写入
+		file.Close();
+	}
+	
+}
+
+//	点击"打开"按钮
+void MainDlg::OnBnClickedButtonOpen()
+{
+	CFileDialog fDlg(TRUE, ".dat", "save", OFN_OVERWRITEPROMPT, "数据文件(*.dat)|*.dat", NULL);
+	if (fDlg.DoModal() == IDOK)
+	{
+		CStdioFile file(fDlg.GetPathName(), CFile::modeRead);
+		CString m_temp;
+		CString m_Name, m_Num, m_Class, m_temp, T_maths, T_cpp;
+		Student Stemp;
+		m_Row = 0;
+		while (file.ReadString(m_temp))
+		{
+
+			int x = m_temp.Find("#");
+			int x2 = m_temp.Find("#", x + 1);
+			int x3 = m_temp.Find("#", x2 + 1);
+			int x4 = m_temp.Find("#", x3 + 1);
+			int x5 = m_temp.Find("#", x4 + 1);
+			int x6 = m_temp.Find("#", x5 + 1);
+			int x7 = m_temp.Find("#", x6 + 1);
+
+			Stemp.Num = m_temp.Left(x);
+			Stemp.Grade = m_temp.Mid(x + 1, x2 - x - 1);
+			Stemp.Class = m_temp.Mid(x2 + 1, x3 - x2 - 1);
+			Stemp.Name = m_temp.Mid(x3 + 1, x4 - x3 - 1);
+			Stemp.Sex = m_temp.Mid(x4 + 1, x5 - x4 - 1);
+			Stemp.maths = _tcstod(m_temp.Mid(x5 + 1, x4 - x3 - 1), NULL);
+			Stemp.CPP = _tcstod(m_temp.Mid(x6 + 1, x7 - x6 - 1), NULL);
+			ManagerSystem.AddData(Stemp);
+
+			m_List.InsertItem(m_Row, Stemp.Num);
+			m_List.SetItemText(m_Row, 1, Stemp.Grade);
+			m_List.SetItemText(m_Row, 2, Stemp.Class);
+			m_List.SetItemText(m_Row, 3, Stemp.Name);
+			m_List.SetItemText(m_Row, 4, Stemp.Sex);
+			CString Format_Gra;
+			Format_Gra.Format("%.1f", Stemp.maths);
+			m_List.SetItemText(m_Row, 5, Format_Gra);
+			Format_Gra.Format("%.1f", Stemp.CPP);
+			m_List.SetItemText(m_Row, 6, Format_Gra);
+
+			m_Row++;
+		}
+		
+	}
+
+	//更新学生人数
+	int nCount = m_List.GetItemCount();
+	m_STU.Format(_T("%d"), nCount);
+	SetDlgItemText(IDC_EDIT_STU, m_STU);
+}
+
+//	点击"绘图"按钮
+void MainDlg::OnBnClickedButtonStatisgraph()
+{
+	// TODO: 在此添加控件通知处理程序代码
+}
+
+//	双击list control的响应事件（将该学生的信息设置到输入框方便进行修改）
+void MainDlg::OnNMDblclkListShow(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	LPNMITEMACTIVATE pNMItemActivate = reinterpret_cast<LPNMITEMACTIVATE>(pNMHDR);
+
+	//	首先得到点击的位置
+	POSITION pos = m_List.GetFirstSelectedItemPosition();
+
+	if (pos == NULL)
+	{
+		MessageBox("请至少选择一项", "提示", MB_ICONEXCLAMATION);
+		return;
+	}
+
+	//	得到行号，通过POSITION转化
+	m_Row = (int)m_List.GetNextSelectedItem(pos);
+
+	//	将该行的多选框勾上
+	m_List.SetCheck(m_Row, TRUE);
 
 	//	获取该位置学生的信息
 	switch (SortID)
 	{
 	case 0:
-		temp = ManagerSystem.Pstu[i];
+		temp = ManagerSystem.Pstu[m_Row];
 		break;
 	case 1:
-		temp = ManagerSystem.SortM[i];
+		temp = ManagerSystem.SortM[m_Row];
 		break;
 	case 2:
-		temp = ManagerSystem.SortC[i];
+		temp = ManagerSystem.SortC[m_Row];
 		break;
 	default:
 		MessageBox("WTF????	How dare are you！");
 		break;
 	}
-	
+
 	//	设置到输入框
-	SetDlgItemText(IDC_EDIT_Name,temp.Name);
+	SetDlgItemText(IDC_EDIT_Name, temp.Name);
 	SetDlgItemText(IDC_EDIT_Num, temp.Num);
 	SetDlgItemText(IDC_EDIT_Class, temp.Class);
 
-	//	设置性别单选框
 	CButton* Cwnd_radio_temp;
-
-	if (m_Sex=="男")
+	//	设置性别单选框
+	if (m_Sex == "男")
 	{
-		//获取单选框男的指针
+		//	获取单选框男的指针
 		Cwnd_radio_temp = (CButton*)GetDlgItem(IDC_RADIO_SexMan);
 		Cwnd_radio_temp->SetCheck(TRUE);
 	}
 	else
 	{
-		//获取单选框女的指针
+		//	获取单选框女的指针
 		Cwnd_radio_temp = (CButton*)GetDlgItem(IDC_RADIO_SexWoman);
 		Cwnd_radio_temp->SetCheck(TRUE);
 	}
 
 	//	设置年级单选框
-	if (m_Gra=="大一")
+	if (m_Gra == "大一")
 		Cwnd_radio_temp = (CButton*)GetDlgItem(IDC_RADIO_Gra1);
 	if (m_Gra == "大二")
 		Cwnd_radio_temp = (CButton*)GetDlgItem(IDC_RADIO_Gra2);
@@ -330,219 +569,10 @@ void MainDlg::OnBnClickedButtonEdit()
 		Cwnd_radio_temp = (CButton*)GetDlgItem(IDC_RADIO_Gra4);
 	Cwnd_radio_temp->SetCheck(TRUE);
 
-	SetDlgItemText(IDC_EDIT_Name, temp.Name);
-	SetDlgItemText(IDC_EDIT_Name, temp.Name);
+	SetDlgItemText(IDC_EDIT_Mathsd, temp.mathsD);
+	SetDlgItemText(IDC_EDIT_Mathst, temp.mathsT);
+	SetDlgItemText(IDC_EDIT_Cppd, temp.CPPD);
+	SetDlgItemText(IDC_EDIT_Cppt, temp.CPPT);
 
-
-
-
-
-
-	//--------------从编辑框获取数据------------------
-	GetDlgItemText(IDC_EDIT_Name, m_Name);
-	GetDlgItemText(IDC_EDIT_Num, m_Num);
-	GetDlgItemText(IDC_EDIT_Class, m_Class);
-
-	GetDlgItemText(IDC_EDIT_Mathsd, m_MathsD);
-	GetDlgItemText(IDC_EDIT_Mathst, m_MathsT);
-	double maths = 0.4 * _tcstod(m_MathsD, NULL) + 0.6 * _tcstod(m_MathsT, NULL);
-
-	GetDlgItemText(IDC_EDIT_Cppd, m_CppD);
-	GetDlgItemText(IDC_EDIT_Cppt, m_CppT);
-	double cpp = 0.4 * _tcstod(m_CppD, NULL) + 0.6 * _tcstod(m_CppT, NULL);
-
-	//-------------判断是否获取了空数据---------------
-	if (m_Name == "" && m_Num && m_CppD && m_CppT && m_MathsD && m_MathsT)
-	{
-		MessageBox(_T("数据不能为空！请检查输入"), _T("提示"), MB_ICONWARNING);
-		return;
-	}
-
-	//获取数据放入对象
-	temp.Num = m_Num;
-	temp.Grade = m_Gra;
-	temp.Name = m_Name;
-	temp.Sex = m_Sex;
-	temp.Class = m_Class;
-	temp.CPP = cpp;
-	temp.maths = maths;
-
-	ManagerSystem.EditData(temp, i);
-	
-	m_List.DeleteItem(i);
-	m_List.InsertItem(i, m_Num);
-	m_List.SetItemText(i, 1, m_Gra);
-	m_List.SetItemText(i, 2, m_Class);
-	m_List.SetItemText(i, 3, m_Name);
-	m_List.SetItemText(i, 4, m_Sex);
-	//将浮点型转为cstring型
-	m_temp.Format(_T("%.1f"), maths);
-	m_List.SetItemText(i, 5, m_temp);
-	m_temp.Format(_T("%.1f"), cpp);
-	m_List.SetItemText(i, 6, m_temp);
-}
-
-void MainDlg::OnBnClickedButtonDelete()
-{
-	// TODO: 在此添加控件通知处理程序代码
-	for (int i = 0; i < m_List.GetItemCount(); i++)
-	{
-		if (m_List.GetCheck(i))
-		{
-			ManagerSystem.DeleteData(i);
-			m_List.DeleteItem(i);
-			i--;
-		}
-	}
-	//更新学生人数
-	int nCount = m_List.GetItemCount();
-	m_STU.Format(_T("%d"), nCount);
-	SetDlgItemText(IDC_EDIT_STU, m_STU);
-}
-
-int s;
-void MainDlg::OnBnClickedRadiomaths()
-{
-	// TODO:  在此添加控件通知处理程序代码
-	s = 0;
-}
-void MainDlg::OnBnClickedRadioCpp()
-{
-	s = 1;
-}
-
-void MainDlg::OnBnClickedButtonSort()
-{
-	m_List.DeleteAllItems();
-	int nCount = ManagerSystem.GetCount();
-	CString temp;
-	switch (s)
-	{
-	case 0:
-		ManagerSystem.SortDataM();
-		
-		for (int i = 0; i < nCount; i++)
-		{
-			m_List.InsertItem(i, ManagerSystem.SortM[i].Num);
-			m_List.SetItemText(i, 1, ManagerSystem.SortM[i].Grade);
-			m_List.SetItemText(i, 2, ManagerSystem.SortM[i].Class);
-			m_List.SetItemText(i, 3, ManagerSystem.SortM[i].Name);
-			m_List.SetItemText(i, 4, ManagerSystem.SortM[i].Sex);
-			temp.Format(_T("%.1f"), ManagerSystem.SortM[i].maths);
-			m_List.SetItemText(i, 5, temp);
-			temp.Format(_T("%.1f"), ManagerSystem.SortM[i].CPP);
-			m_List.SetItemText(i, 6, temp);
-		}
-		//更新学生人数
-		m_STU.Format(_T("%d"), m_List.GetItemCount());
-		SetDlgItemText(IDC_EDIT_STU, m_STU);
-		break;
-	case 1:
-		ManagerSystem.SortDataC();
-
-		for (int i = 0; i < nCount; i++)
-		{
-			m_List.InsertItem(i, ManagerSystem.SortC[i].Num);
-			m_List.SetItemText(i, 1, ManagerSystem.SortC[i].Grade);
-			m_List.SetItemText(i, 2, ManagerSystem.SortC[i].Class);
-			m_List.SetItemText(i, 3, ManagerSystem.SortC[i].Name);
-			m_List.SetItemText(i, 4, ManagerSystem.SortC[i].Sex);
-			temp.Format(_T("%.1f"), ManagerSystem.SortC[i].maths);
-			m_List.SetItemText(i, 5, temp);
-			temp.Format(_T("%.1f"), ManagerSystem.SortC[i].CPP);
-			m_List.SetItemText(i, 6, temp);
-		}
-		//更新学生人数
-		m_STU.Format(_T("%d"), m_List.GetItemCount());
-		SetDlgItemText(IDC_EDIT_STU, m_STU);
-		break;
-	default:
-		break;
-	}
-}
-
-void MainDlg::OnBnClickedButtonSave()
-{
-	// TODO: 在此添加控件通知处理程序代码
-	CFileDialog fDlg(FALSE, ".dat", "save", OFN_OVERWRITEPROMPT, "数据文件(*.dat)|*.dat", NULL);
-	if (fDlg.DoModal()==IDOK)
-	{
-		CString sPath = fDlg.GetPathName();
-		//创建文件对象
-		CStdioFile file(sPath, CFile::modeWrite | CFile::modeCreate);
-		CString temp;
-		for (int i = 0; i < m_List.GetItemCount(); i++)
-		{
-			temp += m_List.GetItemText(i, 0) + "#";
-			temp += m_List.GetItemText(i, 1) + "#";
-			temp += m_List.GetItemText(i, 2) + "#";
-			temp += m_List.GetItemText(i, 3) + "#";
-			temp += m_List.GetItemText(i, 4) + "#";
-			temp += m_List.GetItemText(i, 5) + "#";
-			temp += m_List.GetItemText(i, 6) + "#\n";
-
-			file.WriteString(temp);
-		}
-
-		file.Close();
-	}
-	
-}
-
-void MainDlg::OnBnClickedButtonOpen()
-{
-	// TODO: 在此添加控件通知处理程序代码
-	CFileDialog fDlg(TRUE, ".dat", "save", OFN_OVERWRITEPROMPT, "数据文件(*.dat)|*.dat", NULL);
-	if (fDlg.DoModal() == IDOK)
-	{
-		CStdioFile file(fDlg.GetPathName(), CFile::modeRead);
-		CString temp;
-		CString m_Name, m_Num, m_Class, m_temp, T_maths, T_cpp;
-		Student Stemp;
-		int i = 0;
-		while (file.ReadString(temp))
-		{
-
-			int x = temp.Find("#");
-			int x2 = temp.Find("#", x + 1);
-			int x3 = temp.Find("#", x2 + 1);
-			int x4 = temp.Find("#", x3 + 1);
-			int x5 = temp.Find("#", x4 + 1);
-			int x6 = temp.Find("#", x5 + 1);
-			int x7 = temp.Find("#", x6 + 1);
-
-			Stemp.Num = temp.Left(x);
-			Stemp.Grade = temp.Mid(x + 1, x2 - x - 1);
-			Stemp.Class = temp.Mid(x2 + 1, x3 - x2 - 1);
-			Stemp.Name = temp.Mid(x3 + 1, x4 - x3 - 1);
-			Stemp.Sex = temp.Mid(x4 + 1, x5 - x4 - 1);
-			Stemp.maths = _tcstod(temp.Mid(x5 + 1, x4 - x3 - 1), NULL);
-			Stemp.CPP = _tcstod(temp.Mid(x6 + 1, x7 - x6 - 1), NULL);
-			ManagerSystem.AddData(Stemp);
-
-			m_List.InsertItem(i, Stemp.Num);
-			m_List.SetItemText(i, 1, Stemp.Grade);
-			m_List.SetItemText(i, 2, Stemp.Class);
-			m_List.SetItemText(i, 3, Stemp.Name);
-			m_List.SetItemText(i, 4, Stemp.Sex);
-			CString Format_Gra;
-			Format_Gra.Format("%.1f", Stemp.maths);
-			m_List.SetItemText(i, 5, Format_Gra);
-			Format_Gra.Format("%.1f", Stemp.CPP);
-			m_List.SetItemText(i, 6, Format_Gra);
-
-			i++;
-		}
-		
-	}
-
-	//更新学生人数
-	int nCount = m_List.GetItemCount();
-	m_STU.Format(_T("%d"), nCount);
-	SetDlgItemText(IDC_EDIT_STU, m_STU);
-}
-
-void MainDlg::OnBnClickedButtonStatisgraph()
-{
-	// TODO: 在此添加控件通知处理程序代码
+	*pResult = 0;
 }
--- STUSM/MainDlg.h
+++ STUSM/MainDlg.h
@@ -3,6 +3,9 @@
 #include "afxwin.h"
 #include <cstring>
 #include "afxdialogex.h"
+#include "Management.h"
+#include "Student.h"
+
 
 
 // MainDlg 对话框
@@ -44,16 +47,25 @@ public:
 	afx_msg void OnBnClickedButtonSave();
 	afx_msg void OnBnClickedButtonOpen();
 	afx_msg void OnBnClickedButtonStatisgraph();
+	afx_msg void OnNMDblclkListShow(NMHDR* pNMHDR, LRESULT* pResult);
 
-	CListCtrl m_List;	// 显示学生详细成绩的列表
-	CString m_STU;	// 当前已添加的学生人数
-	CString m_Num;	//	绑定学号输入框的变量
-	CString m_Name;	// 绑定姓名输入框的变量
-	CString m_Class;	// 绑定班级输入框的变量
-	CString m_Gra;		//	年级
-	CString m_Sex;		//	性别
+	CListCtrl m_List;		// 显示学生详细成绩的列表
+	CString m_STU;		// 当前已添加的学生人数
+	CString m_Num;		//	绑定学号输入框的变量
+	CString m_Name;		// 绑定姓名输入框的变量
+	CString m_Class;		// 绑定班级输入框的变量
+	CString m_Gra;			//	年级
+	CString m_Sex;			//	性别
 	CString m_MathsD;	// 绑定高数平时成绩输入框的变量
 	CString m_MathsT;	// 绑定高数考试成绩输入框的变量
 	CString m_CppD;		//	绑定CPP平时成绩输入框的变量
 	CString m_CppT;		//	绑定CPP考试成绩输入框的变量
+
+	int m_Row;				//	list contorl控件的行号
+	int m_Sort;				//	用于判断按下“排序”按钮时执行什么排序
+	int SortID;					//	用于判断当前列表状态（正常/按高数/按CPP）
+
+	Management ManagerSystem;		//	管理系统的对象
+	Student temp;			//	Student类型的临时对象
+	CString m_temp;		//	cstring类型的临时变量 主要用于承接 转换数据类型等等
 };
